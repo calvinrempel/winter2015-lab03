ef0bb9c0b560da396d49d12db7dde82495b3bbe6
Calvin <Calvin@Calvin_Laptop>
2015-01-21 12:35:28 -0800

    -Added wise/bingo route and Bingo controller
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -42,6 +42,7 @@ $route['sleep'] = "first/zzz";
 $route['lock/(:any)/(:any)'] = "welcome/shucks";
 $route['show/(:num)'] = "first/gimme/$1";
 $route['dunno'] = "guess";
+$route['([a-zA-Z]{4})/bingo'] = "bingo";
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
---
+++ application/controllers/Bingo.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This controller displays the first quote on record.
+ */
+class Bingo extends Application {
+    
+    /**
+     * The default behaviour of First is to show the first quote on record.
+     */
+    public function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(5);
+        $this->data = array_merge( $this->data, $source );
+
+        $this->render();
+    }
+}
+
